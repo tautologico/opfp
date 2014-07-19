a366b2459c5427740228f515d445f685745d272d
Andrei Formiga <archimedes_siracusa@hotmail.com>
2014-07-18 22:48:45 -0300

    Expressoes, interpretacao, maquina de pilha

--- exp/exp.ml
+++ exp/exp.ml
@@ -5,10 +5,60 @@
 
  *)
 
+(** Expressoes aritmeticas *)
 type exp = 
-    Const of int 
+  | Const of int 
   | Adic of exp * exp 
   | Sub of exp * exp
   | Mult of exp * exp
 
+(** Interpretador para expressoes *)
+let rec eval e = 
+  match e with
+    Const n -> n
+  | Adic (e1, e2) -> eval e1 + eval e2
+  | Sub (e1, e2) -> eval e1 - eval e2
+  | Mult (e1, e2) -> eval e1 * eval e2
 
+(** Uma maquina de pilha para executar expressoes *)
+type operacao = OpSoma | OpSub | OpMult
+
+type instrucao = 
+  | EmpConst of int
+  | Oper of operacao
+
+type programa = instrucao list 
+type pilha = int list 
+
+let operandos p = 
+  match p with
+  | [] -> None
+  | _ :: [] -> None
+  | n1 :: n2 :: r -> Some ((n1, n2), r)
+
+let oper o = 
+  match o with
+  | OpSoma -> (+)
+  | OpSub -> (-)
+  | OpMult -> ( * )
+
+(** Execucao de instrucoes da maquina de pilha *)
+let exec_inst p inst = 
+  match inst with
+    EmpConst n -> n :: p
+  | Oper o -> 
+     match operandos p with
+       None -> p   (* mantem a mesma pilha se alguma operacao deu errado *)
+     | Some ((n1, n2), r) -> 
+        let op = oper o in
+        (op n1 n2) :: r
+
+(** Execucao de programas da maquina de pilha *)
+let rec exec_prog p = 
+  List.fold_left exec_inst [] p 
+
+(* TODO compilacao de expressoes para maquina de pilha *)  
+
+(* TODO otimizacao *)
+
+(* TODO traducao para ILasm *)
