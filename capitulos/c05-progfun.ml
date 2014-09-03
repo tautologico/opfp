db506226a623f3b002178258b16281d67771c2db
Andrei Formiga <archimedes_siracusa@hotmail.com>
2014-09-02 22:25:53 -0300

    Conclusao dos exemplos do Cap. 5

--- capitulos/c05-progfun.ml
+++ capitulos/c05-progfun.ml
@@ -9,18 +9,153 @@
 
 (** 5.1 A essência da programação funcional *)
 
+(* Sem exemplos nesta seção *)
+
+
 (** 5.2 Mutabilidade e outros efeitos *)
+
+(* Sem exemplos nesta seção *)
+
 
 (** 5.3 Programação recursiva *)
 
+let rec soma_lista l = 
+  match l with 
+    [] -> 0
+  | x :: rl -> x + soma_lista rl
+
+(* Entra em loop infinito mas estoura a pilha *)
+let rec loop_infinito x = 
+  x + loop_infinito x
+
+(* Entra em loop infinito mas tem recursão em cauda *)
+let rec loop_infinito2 x = 
+  loop_infinito2 x
+
+let rec fatorial n = 
+  match n with
+    0 -> 1
+  | 1 -> 1
+  | _ -> n * fatorial (n - 1)
+
+let rec fatorial_ac n ac = 
+  match n with
+    0 | 1 -> ac
+  | _ -> fatorial_ac (n-1) (n * ac)
+
+let fatorial_ac_5 = fatorial_ac 5 1
+
+let fatorial n = 
+  let rec fatorial_ac n ac = 
+    match n with
+      0 | 1 -> ac
+    | _ -> fatorial_ac (n-1) (n * ac)
+  in 
+  fatorial_ac n 1
+
+
 (** 5.4 Funções de primeira classe *)
 
+let quadrado_anon_5 = (fun x -> x * x) 5
+
+(* As duas definições seguintes para q são equivalentes *)
+let q = fun x -> x * x
+let q x = x * x
+
+let cria_multiplicador x = fun y -> x * y
+
+let mult5 = cria_multiplicador 5
+
+let mult5_7 = mult5 7
+
+let cria_mult_5_11 = cria_multiplicador 5 11
+
+(* As três definições seguintes para mult são equivalentes *)
+let mult x y = x * y
+let mult x = fun y -> x * y
+let mult = fun x -> fun y -> x * y
+
+let mult_6_7 = mult 6 7
+
+(* Função de multiplicação sem currying *)
+let mul (x, y) = x * y
+
+let mult5 = ( * ) 5
+
+let lista_x2 = List.map (( * ) 2) [1; 2; 3; 4; 5]
+
+let mult_7_6 = (fun x y -> x * y) 7 6
+
+
 (** 5.5 Padrões de recursividade *)
+
+(* Mapeamento *)
+let rec quadrado_lista l = 
+  match l with
+    [] -> []
+  | n :: rl -> (n * n) :: quadrado_lista rl
+
+let rec maiusculas_lista ls = 
+  match ls with
+    [] -> []
+  | s :: rls -> String.capitalize s :: maiusculas_lista rls
+
+let rec map f l = 
+  match l with
+    [] -> []
+  | x :: rl -> f x :: map f rl
+
+let quadrado_lista l = map (fun x -> x * x) l
+
+let maiusculas_lista l = map String.capitalize l
+
+let quad_lista = map (fun x -> x * x) [1; 2; 3; 4; 5]
+
+let tam_lista = map String.length ["Alys"; "Myau"; "Tyrone"; "Noah (Lutz)"]
+
+(* Filtragem *)
+let par n = (n mod 2) = 0
+
+let rec pares_lista l = 
+  match l with
+    [] -> []
+  | n :: rl when par n -> n :: pares_lista rl
+  | n :: rl -> pares_lista rl
+
+let pares_lista_1 = pares_lista [3; 11; 26; 13; 2; 0]
+
+let rec filtrar p l = 
+  match l with
+    [] -> []
+  | x :: rl when p x -> x :: filtrar p rl
+  | x :: rl -> filtrar p rl
+
+let filtra_par_1 = filtrar par [3; 11; 26; 13; 2; 0]
+
+(* Redução (fold) *)
+let rec soma_lista l = 
+  match l with 
+    [] -> 0
+  | x :: rl -> x + soma_lista rl
+
+let rec reducao f i l = 
+  match l with 
+    [] -> i
+  | x :: rl -> f x (reducao f i rl)
+
+let reducao_soma = reducao (+) 0 [1; 2; 3; 4]
+
+let rec reducao_esq f i l = 
+  match l with
+    [] -> i
+  | x :: rl -> reducao_esq f (f i x) rl
+
 
 (** 5.6 Tipos como fonte de informação *)
 
 (* Função identidade *)
 let g x = x
+
 
 (** 5.7 Dois operadores para aplicar funções *)
 
@@ -49,7 +184,7 @@ let rec map_arvore f a =
 
 let a2 = No (No (Folha, 17, Folha), 21, No (Folha, 42, Folha))
 
-let a2_x2 = map_arvore (fun x -> x * 2) a2
+let a2_x2 = map_arvore (( * ) 2) a2
 
 let rec fold_arvore f arv ini = 
   match arv with
