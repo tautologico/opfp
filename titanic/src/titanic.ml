67280112333d9b2e043aa93ce630eaa67f9b82d5
Andrei Formiga <archimedes_siracusa@hotmail.com>
2014-09-29 23:41:54 -0300

    Ajustes refletindo mudancas no livro

--- titanic/src/titanic.ml
+++ titanic/src/titanic.ml
@@ -33,11 +33,22 @@ let dados_em_falta csv =
   in
   List.concat @@ List.mapi linha csv 
 
+let dados_em_falta csv = 
+  let coluna i j el = if el = "" then [(i, j)] else [] in 
+  let linha i lin = 
+    lin |> List.mapi (coluna i) |> List.concat
+  in
+  csv |> List.mapi linha |> List.concat 
+
 let colunas_em_falta csv = 
   let faltas = dados_em_falta csv in
-  let module IntSet = Set.Make (struct type t = int let compare = compare end) in
+  let module IntSet = 
+    Set.Make (struct type t = int let compare = compare end) 
+  in
   let set_lista s = IntSet.fold (fun el lis -> el :: lis) s [] in
-  List.fold_left (fun set (lin, col) -> IntSet.add col set) IntSet.empty faltas
+  let adiciona_col set (lin, col) = IntSet.add col set in
+  faltas
+  |> List.fold_left adiciona_col IntSet.empty 
   |> set_lista 
 
 
@@ -83,32 +94,39 @@ let ler_dados_treino nome =
     { id = ler_int id; sobreviveu = ler_sobr sobr; classe = ler_classe cls; 
       nome = nome; gen = ler_genero gen; idade = ler_float idade; 
       irmpar = ler_int ip; paisfilhos = ler_int pf; passagem = pass;
-      preco = ler_float prec; cabine = cab; embarque = ler_embarq emb } in
+      preco = ler_float prec; cabine = cab; embarque = ler_embarq emb } 
+  in
   List.map ler_pass @@ List.tl csv 
 
 (* Dados de teste nao contem a coluna de sobrevivencia *)
 let ler_dados_teste nome = 
   let csv = Csv.load nome in
-  let ler_pass [id; cls; nome; gen; idade; ip; pf; pass; prec; cab; emb] = 
+  let ler_pass [id; cls; nom; gen; idad; ip; pf; pass; prec; cab; emb] = 
     { id = ler_int id; sobreviveu = false; classe = ler_classe cls; 
-      nome = nome; gen = ler_genero gen; idade = ler_float idade; 
+      nome = nom; gen = ler_genero gen; idade = ler_float idad; 
       irmpar = ler_int ip; paisfilhos = ler_int pf; passagem = pass;
-      preco = ler_float prec; cabine = cab; embarque = ler_embarq emb } in
+      preco = ler_float prec; cabine = cab; embarque = ler_embarq emb } 
+  in
   List.map ler_pass @@ List.tl csv 
 
 
 (*** Exploração dos dados ***)
 
+let homem p = p.gen <> Some Fem
+let mulher p = p.gen = Some Fem
+
 let apenas_homens d = 
-  List.filter (fun p -> p.gen <> Some Fem) d 
+  List.filter homem d 
 
 let apenas_mulheres d = 
-  List.filter (fun p -> p.gen = Some Fem) d 
+  List.filter mulher d 
 
 (* calcula a taxa de sobrevivencia de passageiros que satisfazem um predicado *)
 let taxa_sobrev_pred pred d = 
   let pass = List.filter pred d in 
-  let sobr = List.fold_left (fun s p -> if p.sobreviveu then s + 1 else s) 0 pass in
+  let sobr = 
+    List.fold_left (fun s p -> if p.sobreviveu then s + 1 else s) 0 pass 
+  in
   (float sobr) /. (float @@ List.length pass)
 
 (* calcula a taxa de sobrevivencia geral *)
@@ -117,7 +135,9 @@ let taxa_sobrevivencia d =
 
 (* Um classificador simples *)
 let sobrevivencia_por_genero d = 
-  List.map (fun p -> if p.gen = Some Fem then (p.id, 1) else (p.id, 0)) d 
+  d |> List.map (fun p -> 
+                 if mulher p then (p.id, 1) 
+                 else (p.id, 0))
 
 let escreve_resultado res nome = 
   let arqout = open_out nome in
