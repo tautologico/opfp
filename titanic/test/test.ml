dc3b9dec40ee101d03d5fe928da2e2710f81543f
Andrei Formiga <archimedes_siracusa@hotmail.com>
2014-10-09 22:28:26 -0300

    Testes com calculo de entropia

--- titanic/test/test.ml
+++ titanic/test/test.ml
@@ -7,14 +7,14 @@ let p1 = {
   sobreviveu = false;
   classe = Terceira;
   nome = "Jack";
-  genero = Some Masc;
-  idade = 20;
+  gen = Some Masc;
+  idade = 20.0;
   irmpar = 0;
   paisfilhos = 0;
   passagem = "Clandestino";
   preco = 0.0;
   cabine = "C1";
-  embarque = Southampton
+  embarque = Some Southampton
 }
 
 let p2 = { 
@@ -22,14 +22,35 @@ let p2 = {
   sobreviveu = true;
   classe = Primeira;
   nome = "Rose";
-  genero = Some Fem;
-  idade = 21;
+  gen = Some Fem;
+  idade = 21.0;
   irmpar = 0;
   paisfilhos = 0;
   passagem = "A2";
   preco = 10.0;
   cabine = "A1";
-  embarque = Cherbourg
+  embarque = Some Cherbourg
 }
 
 (* Patrick e fulano de Downton Abbey *)
+
+let passageiros = [p1; p2]
+
+let sobreviventes = List.filter (fun p -> p.sobreviveu) passageiros
+
+let mortos = List.filter (fun p -> not p.sobreviveu) passageiros
+
+(* testes de unidade *)
+let t_entropia ctxt = 
+  List.iter (assert_equal ~cmp:(cmp_float ~epsilon:0.001) 0.0)
+            [entropia sobreviventes; entropia mortos]
+
+(* a suite de testes *)
+let suite = 
+  "suite" >:::
+    ["calculo de entropia" >:: t_entropia]
+
+(* executor dos testes *)
+let () = 
+  run_test_tt_main suite
+
