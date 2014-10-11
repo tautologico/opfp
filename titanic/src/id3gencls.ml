d78298401d00aa187c30672a2e963811a3ce6038
Andrei Formiga <archimedes_siracusa@hotmail.com>
2014-10-11 03:24:50 -0300

    Valor default para ID3

--- titanic/src/id3gencls.ml
+++ titanic/src/id3gencls.ml
@@ -8,9 +8,10 @@
  *
  *)
 
+let testes = [Titanic.testa_classe; Titanic.testa_genero]
+
 let () = 
   Printf.printf "Lendo dados de treinamento e construindo arvore\n";
-  let testes = [Titanic.testa_classe; Titanic.testa_genero] in
   let arvore = Titanic.arvore_testes "data/train.csv" testes in
   Printf.printf "Lendo dados de teste e escrevendo gencls.csv\n";
   Titanic.classifica_teste_arvore arvore "data/test.csv" "gencls.csv"
