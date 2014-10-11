38f893858f32d63e001e3c6911389b10614807fc
Andrei Formiga <archimedes_siracusa@hotmail.com>
2014-10-11 00:48:01 -0300

    Reorganizacao do codigo

--- titanic/src/id3gencls.ml
+++ titanic/src/id3gencls.ml
@@ -1,7 +1,8 @@
 (* 
  *
  * id3gencls.ml
- * Constroi uma arvore de decisao usando ID3 e testando genero e classe.
+ * Constroi uma arvore de decisao usando ID3 com testes para genero e classe.
+ * Classifica os dados de teste usando a arvore construida. 
  *
  * Andrei de A. Formiga, 2014-10-04
  *
@@ -9,9 +10,7 @@
 
 let () = 
   Printf.printf "Lendo dados de treinamento e construindo arvore\n";
-  let d_treino = Titanic.ler_dados_treino "data/train.csv" in
-  let arvore = Titanic.id3 d_treino [Titanic.testa_classe; Titanic.testa_genero] in
+  let testes = [Titanic.testa_classe; Titanic.testa_genero] in
+  let arvore = Titanic.arvore_testes "data/train.csv" testes in
   Printf.printf "Lendo dados de teste e escrevendo gencls.csv\n";
-  let d_teste = Titanic.ler_dados_teste "data/test.csv" in
-  let resultado = Titanic.aplica_arvore_dados arvore d_teste in
-  Titanic.escreve_resultado resultado "gencls.csv"
+  Titanic.classifica_teste_arvore arvore "data/test.csv" "gencls.csv"
