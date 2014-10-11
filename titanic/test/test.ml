d2cda8c9209e285961e72144fa10b765d6447942
Andrei Formiga <archimedes_siracusa@hotmail.com>
2014-10-10 23:04:35 -0300

    Alguns testes

--- titanic/test/test.ml
+++ titanic/test/test.ml
@@ -32,9 +32,24 @@ let p2 = {
   embarque = Some Cherbourg
 }
 
+let p3 = { 
+  id = 3;
+  sobreviveu = false;
+  classe = Primeira;
+  nome = "Crowley, Patrick";
+  gen = Some Masc;
+  idade = 22.0;
+  irmpar = 0;
+  paisfilhos = 0;
+  passagem = "A1";
+  preco = 12.0;
+  cabine = "A2";
+  embarque = Some Queenstown
+}
+
 (* Patrick e fulano de Downton Abbey *)
 
-let passageiros = [p1; p2]
+let passageiros = [p1; p2; p3]
 
 let sobreviventes = List.filter (fun p -> p.sobreviveu) passageiros
 
@@ -47,8 +62,17 @@ let t_entropia ctxt =
 
 let t_max_f ctxt = 
   let l1 = [1.1; 0.9; 2.4; 0.2; 3.7; 5.8; 0.3] in
-  let m, _ = max_f (fun x -> x *. x) l1 in
-  assert_equal ~cmp:(cmp_float ~epislon:0.0001) m 5.8
+  let mi, max = max_f (fun x -> x *. x) l1 in
+  assert_equal mi 5
+
+let t_remove_indice ctxt = 
+  let l1 = [1; 2; 3; 4; 5; 6] in
+  let l2 = [11] in
+  assert_equal (remove_indice 0 l1) [2; 3; 4; 5; 6];
+  assert_equal (remove_indice 1 l1) [1; 3; 4; 5; 6];
+  assert_equal (remove_indice 4 l1) [1; 2; 3; 4; 6];
+  assert_equal (remove_indice 5 l1) [1; 2; 3; 4; 5];
+  assert_equal (remove_indice 0 l2) []
 
 let t_particao_lista ctxt = 
   let h1 = Hash.create 10 in
@@ -60,13 +84,33 @@ let t_particao_lista ctxt =
   assert_bool "[3; 7] deve aparecer na lista convertida da tabela hash" (List.mem [3; 7] l1);
   assert_bool "[2; 2] deve aparecer na lista convertida da tabela hash" (List.mem [2; 2] l1)
 
+let t_particao ctxt = 
+  let part_gen = particao_lista @@ particao testa_genero passageiros in
+  assert_bool "[p2] deve aparecer na particao para mulheres" (List.mem [p2] part_gen);
+  assert_bool "[p1; p3] deve aparecer na particao para homens" (List.mem [p1; p3] part_gen)
+
+(* Induz uma arvore apenas com exemplos positivos *)
+let id3_pos ctxt = 
+  let teste p = p.id in
+  let arvore = id3 sobreviventes [teste] in
+  assert_equal arvore (Result true)
+
+(* Induz uma arvore apenas com exemplos negativos *)
+let id3_neg ctxt = 
+  let teste p = p.id in
+  let arvore = id3 mortos [teste] in
+  assert_equal arvore (Result false)
+
 (* a suite de testes *)
 let suite = 
   "suite" >:::
     [
       "calculo de entropia" >:: t_entropia;
       "max_f" >:: t_max_f;
-      "partical_lista" >:: t_particao_lista
+      "particao_indice" >:: t_remove_indice;
+      "partical_lista" >:: t_particao_lista;
+      "id3 sobreviventes" >:: id3_pos;
+      "id3 mortos" >:: id3_neg
     ]
 
 (* executor dos testes *)
